9fca6542484badc2e1b4f8f61d06f06d7e5096dd
Strangedogg <manolodascuie@gmail.com>
2025-12-06 20:55:48 -0300

    Máquinas de estado na tela inicial

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import {Link} from "expo-router";
 import { useState } from "react";
 
 //goes to: gerenciar salas
-//goes to if: 
+//goes to if: (View)
 //        se houverem salas mas não houver um inventário rolando -> importar lista, 
 //        se já houver lista importada e salas -> continuar e substitui a opção de importar pela barra de progresso do inventário
 //        se já houverem inventários concluidos -> relatórios anteriores.
@@ -11,18 +11,44 @@ import { useState } from "react";
 
 export default function Index() {
   
-  let salas = true;       // salas criadas?
-  let lista = true;       // lista importada?
-  let inventarios = true; // inventários concluidos?
+  const [salas, setSalas] = useState(true);
+  const [lista, setLista] = useState(false);
+  const [inventarios, setInventarios] = useState(false);
 
 
   return (
     <View className="flex-1 justify-center items-center">
-      {salas && !lista &&(<Link className="text-4xl text-primary font-bold" href="./Shared/Components/NovoInventario">Novo Inventário</Link>)}
-      {salas  && lista && (<Link className="text-4xl text-primary font-bold" href="./Shared/Components/Inventario">Continuar</Link>)}
-      {inventarios && <Link className="text-4xl text-primary font-bold" href="./Shared/Components/BaixarRelatoriosAnteriores">Relatórios Anteriores</Link>}
+      {salas && !lista && (
+        <TouchableOpacity className="p-5 bg-blue-500 rounded-lg m-2" onPress={() => setLista(!lista)}> 
+          <Text className="text-4xl text-primary font-bold">Novo Inventário</Text>
+        </TouchableOpacity> 
+      )}
+
+      {lista && (
+          <Text className="text-4xl text-primary font-bold">Barra de Progresso</Text>
+      )}
       
-      <Link className="text-4xl text-primary font-bold" href="./Shared/Components/GerenciarSalas">Gerenciar Salas</Link>
+      {salas && lista && (
+        <Link href="/Sala/Shared/Components/Inventario" asChild>
+          <TouchableOpacity className="p-5 bg-blue-500 rounded-lg m-2">
+            <Text className="text-4xl text-primary font-bold">Inventário</Text>
+          </TouchableOpacity>
+        </Link>
+      )}
+      
+      {inventarios && (
+        <Link href="/Sala/Shared/Components/BaixarRelatoriosAnteriores" asChild>
+          <TouchableOpacity className="p-5 bg-blue-500 rounded-lg m-2">
+            <Text className="text-4xl text-primary font-bold">Relatórios Anteriores</Text>
+          </TouchableOpacity>
+        </Link>
+      )}
+      
+      <Link href="/Sala/Shared/Components/GerenciarSalas" asChild>
+        <TouchableOpacity className="p-5 bg-blue-500 rounded-lg m-2">
+          <Text className="text-4xl text-primary font-bold">Gerenciar Salas</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
